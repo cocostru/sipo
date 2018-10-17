bb73e2c5df22b25cb962a6828fc6501cb0031e10
cocostru <dk.pochtamp@gmail.com>
2018-10-17 20:01:41 +0800

    lazy

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -65,7 +65,7 @@ AppAsset::register($this);
     ?>
 
     <div class="container">
-        <?//= Breadcrumbs::widget(['links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [], ]) ?>
+        <?php //echo Breadcrumbs::widget(['links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [], ]) ?>
         <?= Alert::widget() ?>
         <?= $content ?>
     </div>
