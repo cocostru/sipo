98bdef24abc23870ceb9d588ef537493e301ea63
cocostru <dk.pochtamp@gmail.com>
2018-10-17 19:39:41 +0800

    lazy init

---
+++ frontend/views/site/index.php
@@ -0,0 +1,20 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Siberian Power Ltd.';
+?>
+
+
+    <div class="title">
+        <h1>SiberianPower Ltd.</h1><br/>
+    </div>
+
+    <div class="idea">
+        <h2>Providing the structure & interactive functionality <br/>for hi-tech related business & minimizing any of <br/>your operational activity.</h2>
+    </div>
+
+    <div class="mail">
+        <h2> <a href="mailto:info@siberianpower.io?subject=Informational support. SiberianPower" target="_blank"> info@siberianpower . io </a> </h2>
+    </div>
+
