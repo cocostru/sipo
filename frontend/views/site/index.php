f9808690d0fc3a7eb2da8cc5c6da8ff3c42f2b58
cocostru <dk.pochtamp@gmail.com>
2018-10-17 19:48:25 +0800

    lazy

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -5,16 +5,14 @@
 $this->title = 'Siberian Power Ltd.';
 ?>
 
+<div class="title">
+    <h1>SiberianPower Ltd.</h1><br/>
+</div>
 
-    <div class="title">
-        <h1>SiberianPower Ltd.</h1><br/>
-    </div>
+<div class="idea">
+    <h2>Providing the structure & interactive functionality <br/>for hi-tech related business & minimizing any of <br/>your operational activity.</h2>
+</div>
 
-    <div class="idea">
-        <h2>Providing the structure & interactive functionality <br/>for hi-tech related business & minimizing any of <br/>your operational activity.</h2>
-    </div>
-
-    <div class="mail">
-        <h2> <a href="mailto:info@siberianpower.io?subject=Informational support. SiberianPower" target="_blank"> info@siberianpower . io </a> </h2>
-    </div>
-
+<div class="mail">
+    <h2> <a href="mailto:info@siberianpower.io?subject=Informational support. SiberianPower" target="_blank"> info@siberianpower . io </a> </h2>
+</div>
